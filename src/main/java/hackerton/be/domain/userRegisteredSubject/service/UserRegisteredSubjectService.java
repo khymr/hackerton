b9473b2a19452ab687140588ca3a03843d9fcd1f
khymr <20011899@sju.ac.kr>
2024-12-26 17:28:30 +0900

    사용자 별 선택한 과목 추가하는 로직

---
+++ src/main/java/hackerton/be/domain/userRegisteredSubject/service/UserRegisteredSubjectService.java
@@ -0,0 +1,28 @@
+package hackerton.be.domain.userRegisteredSubject.service;
+
+import hackerton.be.domain.subject.Subject;
+import hackerton.be.domain.user.User;
+import hackerton.be.domain.userRegisteredSubject.UserRegisteredSubject;
+import hackerton.be.domain.userRegisteredSubject.repository.UserRegisteredSubjectRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+@Transactional
+public class UserRegisteredSubjectService {
+    private final UserRegisteredSubjectRepository userRegisteredSubjectRepository;
+
+    public void registerSubjects(Long userId, List<Long> subjectIds) {
+        for (Long subjectId : subjectIds) {
+            UserRegisteredSubject userRegisteredSubject = UserRegisteredSubject.builder()
+                    .user(User.builder().id(userId).build())
+                    .subject(Subject.builder().id(subjectId).build())
+                    .build();
+            userRegisteredSubjectRepository.save(userRegisteredSubject);
+        }
+    }
+}
